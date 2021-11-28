32867a753466f548ff36cc7570fcec9795002484
roger.miranda <roger.miranda@students.salle.url.edu>
2021-11-28 14:19:52 +0100

    Feature: linked list; Fix: stop Atreides while users; Feature: join ended users

--- Atreides/Threads.c
+++ Atreides/Threads.c
@@ -5,15 +5,82 @@ typedef struct {
 	void *args;
 } Thread;
 
-int num_threads = 0;
-pthread_t *threads = NULL;
+struct _Node {
+	pthread_t thread;
+	struct _Node *next;
+	bool hasEnded;
+};
+typedef struct _Node Node;
+
+Node *head = NULL;
+
+/**
+ * Afegeix un thread a la llista
+ * @return 		Punter a la variable thread de la llista
+ */
+pthread_t *getNewNode() {
+	Node *node = (Node*)malloc(sizeof(Node));
+	node->hasEnded = false;
+	node->next = head;
+	head = node; // TODO protegir
+	return &node->thread;
+}
+
+/**
+ * Estableix un thread com acabat
+ * @param thread	Valor de pthread_self()
+ * @retval 0		OK
+ * @retval -1		No exiteix a la linked list
+ */
+int setThreadEnded(pthread_t thread) {
+	// TODO protegir [que pasa si mentre s'executa terminateThreads() algu acaba?]
+	Node *current = head;
+	while(current != NULL) {
+		if (current->thread == thread) {
+			current->hasEnded = true;
+			return 0;
+		}
+		current = current->next;
+	}
+	return -1;
+}
+
+/**
+ * Elimina un node de la llista
+ * @param node	Node a eliminar
+ * @param pre	Node previ al node a eliminar [NULL si head]
+ */
+void removeFromList(Node *node, Node *pre) {
+	if (pre == NULL) head = node->next;
+	else pre->next = node->next;
+	free(node);
+}
+
+void gc() {
+	Node *current = head, *pre = NULL, *tmp;
+	while(current != NULL) {
+		tmp = current->next;
+		if (current->hasEnded) {
+			pthread_join(current->thread, NULL);
+			removeFromList(current, pre);
+		}
+		else pre = current;
+		current = tmp;
+	}
+}
 
 void terminateThreads() {
-	while(num_threads > 0) {
-		pthread_exit(&threads[num_threads-1]);
-		num_threads--;
+	gc();
+	
+	Node *current = head, *next;
+	head = NULL; // TODO protegir (entre el 'current = head' i 'head = NULL')
+	while(current != NULL) {
+		// TODO join
+		next = current->next;
+		free(current);
+		
+		current = next;
 	}
-	free(threads);
 }
 
 /**
@@ -26,17 +93,15 @@ void *threadManager(void *args) {
 	Thread thread_args = *((Thread*)args);
 	free(args);
 	void *r = (*thread_args.function)(thread_args.args);
-	// TODO eliminar de la linked list
+	setThreadEnded(pthread_self());
 	return r;
 }
 
 int createThread(void *(*function)(void *), void *args, size_t args_size) {
-	Thread *thread_args = (Thread*)malloc(sizeof(Thread));
+	Thread *thread_args = (Thread*)malloc(sizeof(Thread)); // threadManager fa el free
 	thread_args->function = function;
 	thread_args->args = malloc(args_size);
 	memcpy(thread_args->args, args, args_size);
 	
-	// TODO fer-ho en linked list
-	threads = (pthread_t *)realloc(threads, sizeof(pthread_t)*(++num_threads));
-	return pthread_create(&threads[num_threads-1], NULL, &threadManager, thread_args /* TODO la variable es destruirà abans de ser llegida? */);
+	return pthread_create(getNewNode(), NULL, &threadManager, thread_args /* TODO la variable es destruirà abans de ser llegida? */);
 }
--- Atreides/Threads.h
+++ Atreides/Threads.h
@@ -3,6 +3,12 @@
 #include <pthread.h>	// pthread_create/pthread_join/pthread_t
 #include <stdlib.h>		// malloc/free
 #include <string.h>		// memccpy
+#include <stdbool.h>	// true/false/bool
+
+/**
+ * Uneix els threads que ja han acabat i els elimina de la llista
+ */
+void gc();
 
 /**
  * Força l'aturada de tots els threads oberts
@@ -12,7 +18,7 @@ void terminateThreads();
 /**
  * Crea un nou thread
  * @param function	Funció a cridar pel thread [ha de fer free d'args]
- * @param args		Arguments a entregar al nou thread [no pot contenir cap punter]
+ * @param args		Arguments a entregar al nou thread
  * @param args_size Mida del contingut de 'args'
  * @return			Retorn del pthread_create
  */
