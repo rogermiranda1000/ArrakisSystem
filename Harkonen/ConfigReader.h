60c98be77df6001d071af613425c3d9ec247c319
Carles <carles.roch@onlean.com>
2021-12-28 19:34:04 +0100

    Move config reader

---
+++ Harkonen/ConfigReader.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <unistd.h>
+#include <fcntl.h>		// O_RDONLY
+#include <stdlib.h>		// realloc
+#include <stdbool.h>
+#include <string.h>		// strlen
+
+#define DESCRIPTOR_SCREEN 1
+#define DESCRIPTOR_ERROR 2
+
+/**
+ * Llegeix del FileDescriptor fins trobar el delimitador
+ * /!\ El delimitador no es guarda al buffer, però sí s'elimina del FileDescriptor /!\
+ * /!\ Cal fer el free del buffer /!\
+ * @param fd		FileDescriptor on agafar les dades
+ * @param buffer	Array on guardar la informació (amb '\0')
+ * @param delimiter	Caracter que marca quan parar
+ * @return			Caracters obtninguts (sense contar '\0')
+ */
+size_t readUntil(int fd, char **buffer, char delimiter);
+
+/**
+ * Llegeix del File Descriptor tranforman-lo a enter i para a delimiter (agafant-lo)
+ * /!\ El delimitador no es guarda al buffer, però sí s'elimina del FileDescriptor /!\
+ * @param fd 		File Desriptor a consultar
+ * @param stop 		Caracter on s'ha parat la conversió (NULL si no es vol guardar)
+ * @return			Enter trobat (0 si res)
+ */
+int readInteger(int fd, char *stop);
+
+/**
+ * Llegeix un fitxer de configuració, guardant la informació a las variables
+ * /!\ Important fer els frees dels char* /!\
+ * @param name		Ruta del fitxer de configuració
+ * @param timeClean	Temps a esperar abans de netejar els fitxers temporals
+ * @param ip		IP del servidor central
+ * @param port		Port del servidor central
+ * @param directory	Directori de configuració
+ * @retval -1 		Error al obrir el fitxer
+ * @retval 0		OK
+ */
+int readConfig(char* name, unsigned int* timeClean, char** ip, unsigned short* port, char** directory);
