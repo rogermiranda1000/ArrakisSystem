ce56eb61b007394eab84a8d07f6e981d4862bf31
Carles <carles.roch@onlean.com>
2021-12-28 18:11:59 +0100

    Messages

--- Harkonen/harkonen.h
+++ Harkonen/harkonen.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "ProgramLauncher.h"
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h> // exit
+
+#define DESCRIPTOR_SCREEN 1
+#define DESCRIPTOR_ERROR 2
+
+#define STATIC_STRING_LEN(str) (sizeof(str)/sizeof(char))
+
+#define MSG_INIT    "Starting Harkonen\n"
+#define MSG_SEARCH  "Scanning pids\n"
+#define MSG_KILL    "Killing pid "
+#define MSG_EXIT    "Exiting...\n"
