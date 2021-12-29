9b022d04bc89c5aa29aea8f3749198ba1e48a7f2
roger.miranda <roger.miranda@students.salle.url.edu>
2021-12-29 14:30:32 +0100

    Fix: dependencies

--- Harkonen/harkonen.h
+++ Harkonen/harkonen.h
@@ -1,24 +1,25 @@
 #pragma once
 
 #include "ProgramLauncher.h"
-#include "Configreader.h"
+#include "ConfigReader.h"
 
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h> // exit
 #include <unistd.h>
+#include <stdio.h>	// snprintf
 
 #define DESCRIPTOR_SCREEN 1
 #define DESCRIPTOR_ERROR 2
 
 #define STATIC_STRING_LEN(str) (sizeof(str)/sizeof(char))
 
-#define MSG_INIT    "Starting Harkonen\n"
-#define MSG_SEARCH  "Scanning pids\n"
-#define MSG_KILL    "Killing pid "
-#define MSG_EXIT    "Exiting...\n"
+#define MSG_INIT	"Starting Harkonen\n"
+#define MSG_SEARCH	"Scanning pids\n"
+#define MSG_KILL	"Killing pid "
+#define MSG_EXIT	"Exiting...\n"
 
-#define ERR_ARGS    "Please enter one argument stating the number of seconds between deletions.\n\n"
+#define ERROR_ARGS	"Please enter one argument stating the number of seconds between deletions.\n\n"
 
 #define CMD_WHO     "whoami"
 
